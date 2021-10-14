146f00bdc2d347a6a03ec5f6bfb27dfbbe8c6f60
zhao-feifei <398983914@qq.com>
2021-10-14 19:18:22 +0800

    自定义组件方法

--- components/test/test.js
+++ components/test/test.js
@@ -1,5 +1,8 @@
 // components/test/test.js
 Component({
+    options:{
+        styleIsolation:'isolated'
+    },
     /**
      * 组件的属性列表
      */
@@ -11,13 +14,24 @@ Component({
      * 组件的初始数据
      */
     data: {
-
+        count:0
     },
 
     /**
      * 组件的方法列表
      */
     methods: {
-
+        addCount(){
+            this.setData({
+                count:this.data.count+1
+            })
+            this._showCount()
+        },
+        _showCount(){
+            wx.showToast({
+              title: 'count的值是：'+this.data.count,
+              icon:'none'
+            })
+        }
     }
 })
